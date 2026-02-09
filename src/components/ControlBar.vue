34104802882749713cea97423ce4542a5140c7dd
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-02-09 15:04:38 -0500

    lesson finished

---
+++ src/components/ControlBar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const emit = defineEmits<{
+    (event: 'add-count', num: number): void;
+    (event: 'reset-count'): void;
+}>()
+
+</script>
+
+<template>
+    <p>
+        <button @click="emit('add-count', 1)">Add</button>
+        <button @click="emit('reset-count')">Reset</button>
+    </p>
+</template>
