f77043d5617e54ae148a790852258408ac9f2c70
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-02-09 12:13:59 -0500

    lesson finished

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h1>{{ appInfo.name }}</h1>
+    <h2>{{ appInfo.slogan }}</h2>
+  </div>
+  <p>{{ count }}</p>
 </template>
 
-<script lang="ts">
-</script>
+<script setup lang="ts">
+import { ref, reactive, onMounted } from 'vue'
+import fetchCount from './fetchCount'
+interface AppInfo {
+  name: string,
+  slogan: string
+}
 
+const count = ref(0)
+
+const appInfo: AppInfo = reactive({
+  name: 'Counter',
+  slogan: 'Am app you can count',
+})
+
+onMounted(() => {
+  fetchCount((initialCount) => {
+    count.value = initialCount  
+  })
+})
+</script>
